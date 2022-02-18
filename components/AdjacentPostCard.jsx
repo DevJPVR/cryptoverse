6e8a2da46a07e317f91947bbec50399cbc73e0a8
Joao Pedro <dev.joaopedrovieira@gmail.com>
2022-02-18 22:00:26 +0000

    First Commit

--- components/AdjacentPostCard.jsx
+++ components/AdjacentPostCard.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 const AdjacentPostCard = ({ post, position }) => (
   <>
-    <div className="absolute rounded-lg bg-center bg-no-repeat bg-cover shadow-md inline-block w-full h-72" style={{ backgroundImage: `url('${post.featuredImage.url}')` }} />
+    <div className="absolute rounded-lg bg-center bg-no-repeat bg-cover shadow-md inline-block w-full h-72" style={{ backgroundImage: `url('${post.featuredimage.url}')` }} />
     <div className="absolute rounded-lg bg-center bg-gradient-to-b opacity-50 from-gray-400 via-gray-700 to-black w-full h-72" />
     <div className="flex flex-col rounded-lg p-4 items-center justify-center absolute w-full h-full">
       <p className="text-white text-shadow font-semibold text-xs">{moment(post.createdAt).format('MMM DD, YYYY')}</p>
--- components/index.js
+++ components/index.js
@@ -9,3 +9,4 @@ export { default as PostDetail } from './PostDetail';
 export { default as Author } from './Author';
 export { default as CommentsForm } from './CommentsForm';
 export { default as Comments } from './Comments';
+export { default as Loader } from './Loader';
