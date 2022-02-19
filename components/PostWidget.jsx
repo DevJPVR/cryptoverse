4b961a141acff2eb329ef65c43082975ea14d860
Joao Pedro Vieira <dev.joaopedrovieira@gmail.com>
2022-02-19 17:43:50 +0000

    Update PostWidget.jsx

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -22,7 +22,7 @@ const PostWidget = ({ categories, slug }) => {
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">
-        {slug? 'Related Posts' : "Recent Posts"}
+        {slug? 'Posts Relacionados' : "Posts Recentes"}
       </h3>
       {relatedPosts.map((post) => (
         <div key={post.title} className="flex items-center w-full mb-4">
